f3774f55e108a7806c85342d5d71ba8f8c0c9a61
Chris Lewis <chris.lewis2@gmail.com>
2015-01-18 00:20:58 -0500

    Refactored view controller and fixed Tesseract inconsistent OCR bug.

--- Solvr/ViewController.mm
+++ Solvr/ViewController.mm
@@ -7,21 +7,21 @@
 //
 
 #import "ViewController.h"
-
-#import <sstream>
 #import <string>
 #import <TesseractOCR/TesseractOCR.h>
-
 #import "ComputerVision.h"
 #import "NorvigSolver.h"
 #import "EricaSadanCookbook.h"
 
 @implementation ViewController
 
-BOOL boardLoaded;
-NSString* stateCache;
+BOOL usingSampleImage = NO;
 ComputerVision* cv = NULL;
-Tesseract* tesseract = NULL;
+
+// Track webview load status and inputs waiting to be sent
+BOOL boardLoaded = NO;
+NSString* boardToShow = @"";
+NSString* solutionForBoard = @"";
 
 - (void) viewDidLoad {
     [ super viewDidLoad ];
@@ -29,18 +29,11 @@ Tesseract* tesseract = NULL;
     // Board Detection
     cv = [ [ ComputerVision alloc ] init ];
     
-    // OCR
-    NSLog( @"Initialise Tesseract" );
-    tesseract = [ [ Tesseract alloc ] initWithLanguage:@"eng" ];
-    [ tesseract setVariableValue:@"123456789" forKey:@"tessedit_char_whitelist" ]; // only numbers
-    [ tesseract setVariableValue:@"7" forKey:@"tessedit_pageseg_mode" ]; // one character per image
-    
     // Results Board
     NSLog( @"Load HTML Board" );
     self.board.hidden = YES;
     self.board.delegate = self;
     boardLoaded = NO;
-    stateCache = @"";
     NSURL *url = [ [ NSBundle mainBundle ] URLForResource:@"board" withExtension:@"html" ];
     [ self.board loadRequest:[ NSURLRequest requestWithURL:url ] ];
     
@@ -55,12 +48,6 @@ Tesseract* tesseract = NULL;
     AVCaptureSession *session = [ [ AVCaptureSession alloc ] init ];
     session.sessionPreset = AVCaptureSessionPresetMedium;
     
-    // User feedback
-    self.captureVideoPreviewLayer = [ [ AVCaptureVideoPreviewLayer alloc ] initWithSession:session ];
-    self.captureVideoPreviewLayer.frame = self.view.frame;
-    [ self.captureVideoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill ];
-    [ self.backgroundImage.layer addSublayer:self.captureVideoPreviewLayer ];
-    
     // Find camera
     AVCaptureDevice *device = [ AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo ];
     NSError *error = nil;
@@ -74,8 +61,20 @@ Tesseract* tesseract = NULL;
         
         [ session addOutput:self.stillImageOutput ];
         [ session startRunning ];
+        
+        // User feedback (moved inside if statement)
+        self.captureVideoPreviewLayer = [ [ AVCaptureVideoPreviewLayer alloc ] initWithSession:session ];
+        self.captureVideoPreviewLayer.frame = self.view.frame;
+        [ self.captureVideoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill ];
+        [ self.backgroundImage.layer addSublayer:self.captureVideoPreviewLayer ];
     } else {
+        // For the moment load a sample image when the camera
+        // cannot be used. Ideally there would instead be a
+        // message explaining the problem and giving the user
+        // an opportunity to load a sample image.
         NSLog( @"ERROR: trying to open camera: %@", error );
+        self.backgroundImage.image = [ UIImage imageNamed:@"SampleImage" ];
+        usingSampleImage = YES;
     }
 }
 
@@ -88,19 +87,19 @@ Tesseract* tesseract = NULL;
 
 - (void) webViewDidFinishLoad:(UIWebView*)webView {
     boardLoaded = YES; // one would expect this to be covered by `webView.loading`
-    if( ![ stateCache isEqualToString:@"" ] ) {
-        [ self update:stateCache ];
-        stateCache = @"";
+    if( ![ boardToShow isEqualToString:@"" ] ) {
+        [ self update:boardToShow withSolution:solutionForBoard ];
     }
 }
 
-- (void) update:(NSString*)state {
-    stateCache = state;
+- (void) update:(NSString*)board withSolution:(NSString*)solution {
+    boardToShow = board;
+    solutionForBoard = solution;
     if( boardLoaded ) {
-        NSString* js = [ [ NSString alloc ] initWithFormat:@"set_board('%@');", stateCache ];
+        NSString* js = [ [ NSString alloc ] initWithFormat:@"set_board('%@', '%@' );", boardToShow, solutionForBoard ];
         [ self.board stringByEvaluatingJavaScriptFromString:js ];
         self.board.hidden = NO;
-        NSLog( @"board loaded: %@", stateCache );
+        NSLog( @"board loaded: %@", solutionForBoard );
     }
 }
 
@@ -116,63 +115,72 @@ Tesseract* tesseract = NULL;
     
     // disable button to avoid mashing
     self.omnibutton.enabled = NO;
-    
-    // capture frame from camera
-    AVCaptureConnection *videoConnection = [ self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo];
-    [videoConnection setVideoOrientation:AVCaptureVideoOrientationPortrait];
 
-    [self.stillImageOutput
-        captureStillImageAsynchronouslyFromConnection:videoConnection
-        completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error)
-     {
-         if( error ) { // todo: better error
-             NSLog( @"error (lol this is super helpful isn't it.)" );
-         } else {
-             NSData *jpegData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
-             UIImage *image = [UIImage imageWithData:jpegData];
-             image = applyAspectFillImage( image, self.captureVideoPreviewLayer.frame );
-             //image = [ self applyAspectFillImage:image InRect:self.captureVideoPreviewLayer.frame ];
+    if( usingSampleImage ) {
+        NSLog( @"processing image" );
+        [ self processImage:self.backgroundImage.image ];
+    } else { // capture frame from camera
+        AVCaptureConnection *videoConnection = [ self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo];
+        [ videoConnection setVideoOrientation:AVCaptureVideoOrientationPortrait ];
 
-             // Process image
-
-              NSString* flat_board = [ cv recogniseSudokuFromImage:image withOCR:tesseract ];
-             
-             // Basic check for validity of scanned board. Technically this
-             // means the scanned board must have only one complete solution.
-             // An empty board (all zeroes) will have multiple solutions. But
-             // obviously there are other cases where this will occur. A
-             // better solution could be to algorithmically check for
-             // for multiple solutions, perhaps using the Norvig solver.
-             NSLog( @"%@", flat_board );
-         //    flat_board = @"4370680000003008070500050600400010008030506090006000300105000907050060000009 0 56";
-             if( [ flat_board isEqualToString:@"000000000000000000000000000000000000000000000000000000000000000000000000000000000" ] ) {
-                 NSLog( @"No solution: empty board" );
-                 [ self.omnibutton setTitle:@"Solve" forState:UIControlStateNormal ];
+        [ self.stillImageOutput
+         captureStillImageAsynchronouslyFromConnection:videoConnection
+         completionHandler:^( CMSampleBufferRef imageDataSampleBuffer, NSError *error ) {
+             if( error ) { // todo: better error
+                 NSLog( @"error (lol this is super helpful isn't it.)" );
              } else {
-                 // convert to c++ string and feed to solver
-                 Sudoku::init();
-                 auto sc = new Sudoku( std::string( [ flat_board UTF8String ] ) );
-                 
-                 // contradiction detected in puzzle
-                 if( sc->valid ) {
-                     if( auto S = solve( std::unique_ptr<Sudoku>( sc ) ) ) {
-                        [ self update:[ NSString stringWithUTF8String:S->flatten().c_str() ] ];
-                        [ self.omnibutton setTitle:@"Clear" forState:UIControlStateNormal ];
-                    } else {
-                        NSLog( @"No solution: invalid board" );
-                        [ self.omnibutton setTitle:@"Solve" forState:UIControlStateNormal ];
-                    }
-                 } else {
-                     NSLog( @"No solution: contradiction" );
-                     [ self.omnibutton setTitle:@"Solve" forState:UIControlStateNormal ];
-                 }
+                 NSData *jpegData = [ AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer ];
+                 UIImage *image = [ UIImage imageWithData:jpegData ];
+                 image = applyAspectFillImage( image, self.captureVideoPreviewLayer.frame );
+                 [ self processImage:image ];
              }
-             
-             // e-enable button if disabled
-             self.omnibutton.enabled = YES;
          }
-     }
-     ];
+         ];
+    }
+}
+
+- (void) processImage:(UIImage*)image {
+    // Tesseract is initialised here due to an ongoing bug where doing so in
+    // global scope leads to bizzare misreads after second image. This does
+    // harm performance due to wasted cycles re-initialising but the hit
+    // doesn't appear to be noticable.
+    Tesseract* tesseract = [ [ Tesseract alloc ] initWithLanguage:@"eng" ];
+    [ tesseract setVariableValue:@"123456789" forKey:@"tessedit_char_whitelist" ]; // only numbers
+    [ tesseract setVariableValue:@"7" forKey:@"tessedit_pageseg_mode" ]; // one character per image
+    
+    NSString* flat_board = [ cv recogniseSudokuFromImage:image withOCR:tesseract ];
+    
+    // Basic check for validity of scanned board. Technically this
+    // means the scanned board must have only one complete solution.
+    // An empty board (all zeroes) will have multiple solutions. But
+    // obviously there are other cases where this will occur. A
+    // better solution could be to algorithmically check for
+    // for multiple solutions, perhaps using the Norvig solver.
+    NSLog( @"%@", flat_board );
+    if( [ flat_board isEqualToString:@"000000000000000000000000000000000000000000000000000000000000000000000000000000000" ] ) {
+        NSLog( @"No solution: empty board" );
+        [ self.omnibutton setTitle:@"Solve" forState:UIControlStateNormal ];
+    } else {
+        // convert to c++ string and feed to solver
+        Sudoku::init();
+        auto sc = new Sudoku( std::string( [ flat_board UTF8String ] ) );
+        
+        // contradiction detected in puzzle
+        if( sc->valid ) {
+            if( auto S = solve( std::unique_ptr<Sudoku>( sc ) ) ) {
+                [ self update:flat_board withSolution:[ NSString stringWithUTF8String:S->flatten().c_str() ] ];
+                [ self.omnibutton setTitle:@"Clear" forState:UIControlStateNormal ];
+            } else {
+                NSLog( @"No solution: invalid board" );
+                [ self.omnibutton setTitle:@"Solve" forState:UIControlStateNormal ];
+            }
+        } else {
+            NSLog( @"No solution: contradiction" );
+            [ self.omnibutton setTitle:@"Solve" forState:UIControlStateNormal ];
+        }
+    }
+
+    self.omnibutton.enabled = YES; // e-enable button if disabled
 }
 
 @end
